938c648191960a61c4ca70084f99440c53e72091
birkholz-cubert <142295759+birkholz-cubert@users.noreply.github.com>
2024-03-01 10:41:54 +0100

    Feature/add documentation (#1)
    * added initial sphinx config
    
    * working documentation example
    
    * removed some empty files
    
    ---------

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Cuvis AI'
+copyright = '2024, Cubert GmbH'
+author = 'Cubert GmbH'
+release = '3.2.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+	'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# html_extra_path = ['C:\\dev\\builds\\cuvis_doc\\doc\\html']
+
+html_theme_options = {
+
+}

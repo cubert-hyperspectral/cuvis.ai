2af63f09d233cee47bf62a33e94e9172c425c630
Philip Manke <manke@cubert-gmbh.de>
2024-03-01 16:39:11 +0100

    Datasets now use Torchvision

--- cuvis_ai/utils/data/CuvisData.py
+++ cuvis_ai/utils/data/CuvisData.py
@@ -6,9 +6,12 @@ import glob
 import yaml
 import json
 import copy
+from typing import Optional, Callable
 import torch
-from torch.utils.data import Dataset
+from torchvision.datasets import VisionDataset
+from torchvision import tv_tensors
 from imantics import Dataset as Labelparser
+from pycocotools.coco import COCO
 
 from .Metadata import Metadata
 from .NumpyData import NumpyData
@@ -22,26 +25,38 @@ class CuvisData(NumpyData):
             self.path = path
             self.idx = idx
         def __call__(self, to_dtype:np.dtype):
-            return torch.as_tensor(cuvis.SessionFile(self.path).get_measurement(self.idx).Data["cube"].array.astype(to_dtype))
+            cube = cuvis.SessionFile(self.path).get_measurement(self.idx).data["cube"].array
+            print(cube.shape)
+            cube = np.moveaxis(cube, -1, 0)
+            print("After move axis:", cube.shape)
+            return tv_tensors.Image(cube.astype(to_dtype))
     
     class _LegacyCubeLoader:
         def __init__(self, path):
             self.path = path
         def __call__(self, to_dtype:np.dtype):
-            return torch.as_tensor(cuvis.Measurement.load(self.path).Data["cube"].array.astype(to_dtype))
+            cube = cuvis.Measurement.load(self.path).data["cube"].array
+            print(cube.shape)
+            cube = np.moveaxis(cube, -1, 0)
+            print("After move axis:", cube.shape)
+            return tv_tensors.Image(cube.astype(to_dtype))
     
-    def __init__(self, data_directory_path: str):
+    def __init__(self, root: str, 
+        transforms: Optional[Callable] = None,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ):
         self._FILE_EXTENSION_SESSION = ".cu3s"
         self._FILE_EXTENSION_LEGACY = ".cu3"
-        super().__init__(data_directory_path)
+        super().__init__(root, transforms, transform, target_transform)
         
 
     def _load_directory(self, dir_path:str):
         if debug_enabled:
             print("Reading from directory:", dir_path)
-        fileset_session = glob.glob(os.path.join(self.path, '**/*' + self._FILE_EXTENSION_SESSION), recursive=True)
+        fileset_session = glob.glob(os.path.join(self.root, '**/*' + self._FILE_EXTENSION_SESSION), recursive=True)
         
-        fileset_legacy = glob.glob(os.path.join(self.path, '**/*' + self._FILE_EXTENSION_LEGACY), recursive=True)
+        fileset_legacy = glob.glob(os.path.join(self.root, '**/*' + self._FILE_EXTENSION_LEGACY), recursive=True)
         
         for cur_path in fileset_session:
             self._load_session_file(cur_path)
@@ -60,10 +75,10 @@ class CuvisData(NumpyData):
 
         lp:Labelparser = None
         
-        if os.path.isfile(labelpath):
-            lp = Labelparser(filepath)
-            with open(labelpath, "r") as file:
-                lp.from_coco(json.load(file))
+        #if os.path.isfile(labelpath):
+        #    lp = Labelparser(filepath)
+        #    with open(labelpath, "r") as file:
+        #        lp.from_coco(json.load(file))
 
         if self.metadata_filepath:
             sess_meta = Metadata(filepath, self.fileset_metadata)
@@ -73,12 +88,16 @@ class CuvisData(NumpyData):
         temp_mesu = crt_session.get_measurement(0)
         sess_meta.shape = (temp_mesu.data["cube"].width, temp_mesu.data["cube"].height, temp_mesu.data["cube"].channels)
         sess_meta.wavelengths_nm = temp_mesu.data["cube"].wavelength
-        sess_meta.framerate = crt_session.fps
+        #sess_meta.framerate = crt_session.fps
+        
+        if os.path.isfile(labelpath):
+            coco = COCO(labelpath)
+            ids = list(sorted(coco.imgs.keys()))
         
         for idx in range(cube_count):
             cube_path = F"{filepath}:{idx}"
-            self.data[cube_path] = {}
-            self.data[cube_path]["data"] = self._SessionCubeLoader(filepath, idx)
+            self.data_map[cube_path] = {}
+            self.data_map[cube_path]["data"] = self._SessionCubeLoader(filepath, idx)
             
             mesu = crt_session.get_measurement(idx)
             
@@ -90,32 +109,37 @@ class CuvisData(NumpyData):
             meta.references = {}
             for key, val in [(key, mesu.data[key]) for key in mesu.data.keys() if "_ref" in key]:
                 meta.references[key] = val
-            self.data[cube_path]["meta"] = meta
+            self.data_map[cube_path]["meta"] = meta
 
-            if lp is not None:
-                mesu_lp = Labelparser(cube_path)
-                mesu_lp.name = cube_path
+            if coco is not None:
+                self.data_map[cube_path]["labels"] = coco.loadAnns(coco.getAnnIds(ids[idx]))
                 
-                mesu_lp.images[idx] = mesu_lp.images[idx]
-                for k, v in mesu_lp.images[idx].annotations.items():
-                    mesu_lp.annotations[k] = v
-                self.data[cube_path]["labels"] = mesu_lp
-            else:
-                self.data[cube_path]["labels"] = None
+            #if lp is not None:
+            #    mesu_lp = Labelparser(cube_path)
+            #    mesu_lp.name = cube_path
+            #    
+            #    mesu_lp.images[idx] = mesu_lp.images[idx]
+            #    for k, v in mesu_lp.images[idx].annotations.items():
+            #        mesu_lp.annotations[k] = v
+            #    self.data_map[cube_path]["labels"] = mesu_lp
+            #else:
+            #    self.data_map[cube_path]["labels"] = None
 
     def _load_legacy_file(self, filepath:str):
         print("Found file:", filepath)
         path, _ = os.path.splitext(filepath)
         labelpath = path + ".json"
         
-        lp:Labelparser = None
-        
+        #lp:Labelparser = None
+
         if os.path.isfile(labelpath):
-            self.data[path]["labels"] = Labelparser(filepath)
-            with open(labelpath, "r") as file:
-                self.data[path]["labels"].from_coco(json.load(file))
+            coco =  COCO(labelpath)
+            self.data_map[filepath]["labels"] = coco.loadAnns(coco.getAnnIds(list(coco.imgs.keys())[0]))
+        #    self.data_map[path]["labels"] = Labelparser(filepath)
+        #    with open(labelpath, "r") as file:
+        #        self.data_map[path]["labels"].from_coco(json.load(file))
         else:
-            self.data[filepath]["labels"] = None
+            self.data_map[filepath]["labels"] = None
                 
         if self.metadata_filepath:
             meta = Metadata(filepath, self.fileset_metadata)
@@ -126,8 +150,8 @@ class CuvisData(NumpyData):
         meta.shape = (temp_mesu.data["cube"].width, temp_mesu.data["cube"].height, temp_mesu.data["cube"].channels)
         meta.wavelengths_nm = temp_mesu.data["cube"].wavelength
         
-        self.data[cube_path] = {}
-        self.data[cube_path]["data"] = self._LegacyCubeLoader(filepath)
+        self.data_map[cube_path] = {}
+        self.data_map[cube_path]["data"] = self._LegacyCubeLoader(filepath)
         
         meta.integration_time_us = int(mesu.integration_time * 1000)
         meta.flags = {}
@@ -136,6 +160,6 @@ class CuvisData(NumpyData):
         meta.references = {}
         for key, val in [(key, temp_mesu.data[key]) for key in temp_mesu.data.keys() if "_ref" in key]:
             meta.references[key] = val
-        self.data[filepath]["meta"] = meta
+        self.data_map[filepath]["meta"] = meta
 
 
--- cuvis_ai/utils/data/NumpyData.py
+++ cuvis_ai/utils/data/NumpyData.py
@@ -3,16 +3,19 @@ import glob
 import yaml
 import json
 import time
+from typing import Optional, Callable
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torchvision.datasets import VisionDataset 
+from torchvision import tv_tensors
 from imantics import Dataset as Labelparser
+from pycocotools.coco import COCO
 
 from .Metadata import Metadata
 
 debug_enabled = True
 
-class NumpyData(Dataset):
+class NumpyData(VisionDataset):
 
     C_SUPPORTED_DTYPES = (np.float64, np.float32, np.float16, np.complex64, np.complex128, np.int64, np.int32, np.int16, np.int8, np.uint8, np.bool_)
 
@@ -20,15 +23,18 @@ class NumpyData(Dataset):
         def __init__(self, path):
             self.path = path
         def __call__(self, to_dtype:np.dtype):
-            return torch.as_tensor(np.load(self.path).astype(to_dtype))
+            return tv_tensors.Image(np.load(self.path).astype(to_dtype))
     
-    def __init__(self, data_directory_path: str):
+    def __init__(self, root: str, 
+        transforms: Optional[Callable] = None,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ):
+        super().__init__(root, transforms, transform, target_transform)
         self._FILE_EXTENSION = ".npy"
         
-        self.path = data_directory_path
-        
         self.fileset_metadata = {}
-        self.metadata_filepath = os.path.join(data_directory_path, "metadata.yaml")
+        self.metadata_filepath = os.path.join(self.root, "metadata.yaml")
         if os.path.isfile(self.metadata_filepath):
             self.fileset_metadata = yaml.safe_load(open(self.metadata_filepath, "r"))
         else:
@@ -37,14 +43,14 @@ class NumpyData(Dataset):
         self.data_types:set = set()
         self.provide_datatype:np.dtype = np.float32
         
-        self.data:dict = {}
-        self._load_directory(self.path)
+        self.data_map:dict = {}
+        self._load_directory(self.root)
 
     
-    def _load_directory(self, dir_path:str):
+    def _load_directory(self):
         if debug_enabled:
-            print("Reading from directory:", dir_path)
-        fileset = glob.glob(os.path.join(self.path, '**/*' + self._FILE_EXTENSION), recursive=True)
+            print("Reading from directory:", self.root)
+        fileset = glob.glob(os.path.join(self.root, '**/*' + self._FILE_EXTENSION), recursive=True)
 
         for cur_path in fileset:
             self._load_file(cur_path)
@@ -54,16 +60,17 @@ class NumpyData(Dataset):
             print("Found file:", filepath)
         path, _ = os.path.splitext(filepath)
         labelpath = path + ".json"
-        self.data[path] = {}
-        self.data[path]["data"] = self._NumpyLoader_(filepath)
+        self.data_map[path] = {}
+        self.data_map[path]["data"] = self._NumpyLoader_(filepath)
         
         if os.path.isfile(labelpath):
-            lp = Labelparser(filepath)
-            with open(labelpath, "r") as file:
-                lp.from_coco(json.load(file))
-            self.data[path]["labels"] = lp
+#            lp = Labelparser(filepath)
+#            with open(labelpath, "r") as file:
+#                lp.from_coco(json.load(file))
+            coco = COCO(labelpath)
+            self.data_map[path]["labels"] = coco.loadAnns(coco.getAnnIds(list(coco.imgs.keys())[0]))
         else:
-            self.data[path]["labels"] = None
+            self.data_map[path]["labels"] = None
             
         if self.metadata_filepath:
             meta = Metadata(filepath, self.fileset_metadata)
@@ -74,17 +81,24 @@ class NumpyData(Dataset):
         meta.shape = temp_data.shape
         meta.datatype = temp_data.dtype
         self.data_types.add(meta.datatype)
-        self.data[path]["meta"] = meta
+        self.data_map[path]["meta"] = meta
         
 
     def __len__(self):
-        return len(self.data)
+        return len(self.data_map)
 
     def __getitem__(self, idx: int):
-        return list(self.data.values())["data"](self.provide_datatype)
+        data = list(self.data_map.values())[idx]["data"](self.provide_datatype)
+        labels = self.get_labels(idx)
+        if self.transforms is not None:
+            if labels is not None:
+                data, labels = self.transforms(data, labels)
+            else:
+                data = self.transforms(data)
+        return data, self.get_metadata(idx), labels
     
     def update(self, other_dataset):
-        self.data.update(other_dataset.get_all_data())
+        self.data_map.update(other_dataset.get_all_data())
 
     def set_datatype(self, dtype: np.dtype):
         if dtype in C_SUPPORTED_DTYPES:
@@ -92,7 +106,7 @@ class NumpyData(Dataset):
         else:
             raise ValueError("Unsupported data type: {" + str(dtype.name) + " - use one of: " + str([d.name for d in C_SUPPORTED_DTYPES]))
 
-    def random_split(self, train_percent, val_percent, test_percent) -> List[torch.utils.data.dataset.Subset]:
+    def random_split(self, train_percent, val_percent, test_percent) -> list[torch.utils.data.dataset.Subset]:
         gen = torch.torch.Generator().manual_seed(time.time_ns())
         return torch.utils.data.random_split(self, [train_percent, val_percent, test_percent], gen)
     
@@ -103,22 +117,22 @@ class NumpyData(Dataset):
         return self.provide_datatype
     
     def get_all_data(self):
-        return self.data
+        return self.data_map
 
     def get_data(self, idx:int):
-        return list(self.data.items())[idx]
+        return list(self.data_map.items())[idx]
 
     def get_all_items(self):
-        return [i["data"](self.provide_datatype) for i in self.data.values()]
+        return [i["data"](self.provide_datatype) for i in self.data_map.values()]
     
     def get_item(self, idx:int):
-        return list(self.data.values())[idx]["data"](self.provide_datatype)
+        return self.__getitem__(idx)
 
     def get_all_metadata(self):
-        return [i["meta"] for i in self.data.values()]
+        return [i["meta"] for i in self.data_map.values()]
 
     def get_metadata(self, idx:int):
-        return list(self.data.values())[idx]["meta"]
+        return list(self.data_map.values())[idx]["meta"]
 
     def get_all_labels(self):
         def _get_labels(v):
@@ -126,11 +140,11 @@ class NumpyData(Dataset):
                 return v["labels"]
             except KeyError:
                 return None
-        return [_get_labels(i) for i in self.data.values()]
+        return [_get_labels(i) for i in self.data_map.values()]
 
     def get_labels(self, idx:int):
         try:
-            return list(self.data.values())[idx]["labels"]
+            return list(self.data_map.values())[idx]["labels"]
         except KeyError:
             return None
 

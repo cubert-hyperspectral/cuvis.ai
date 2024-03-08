d3dc4216fa506ec4f6d51ff85b6e8076ad235520
Philip Manke <manke@cubert-gmbh.de>
2024-03-08 15:41:04 +0100

    Disable/remove some debug output; Fix legacy file loader; Disable meta-data scanning for now

--- cuvis_ai/utils/data/CuvisData.py
+++ cuvis_ai/utils/data/CuvisData.py
@@ -26,9 +26,7 @@ class CuvisData(NumpyData):
             self.idx = idx
         def __call__(self, to_dtype:np.dtype):
             cube = cuvis.SessionFile(self.path).get_measurement(self.idx).data["cube"].array
-            print(cube.shape)
             cube = np.moveaxis(cube, -1, 0)
-            print("After move axis:", cube.shape)
             return tv_tensors.Image(cube.astype(to_dtype))
     
     class _LegacyCubeLoader:
@@ -36,9 +34,7 @@ class CuvisData(NumpyData):
             self.path = path
         def __call__(self, to_dtype:np.dtype):
             cube = cuvis.Measurement.load(self.path).data["cube"].array
-            print(cube.shape)
             cube = np.moveaxis(cube, -1, 0)
-            print("After move axis:", cube.shape)
             return tv_tensors.Image(cube.astype(to_dtype))
     
     def __init__(self, root: str, 
@@ -64,14 +60,16 @@ class CuvisData(NumpyData):
             self._load_legacy_file(cur_path)
             
     def _load_session_file(self, filepath: str):
-        print("Found file:", filepath)
+        if debug_enabled:
+            print("Found file:", filepath)
         path, _ = os.path.splitext(filepath)
         labelpath = path + ".json"
 
         crt_session = cuvis.SessionFile(filepath)
 
         cube_count = len(crt_session)
-        print("Session file has", cube_count, "cubes")
+        if debug_enabled:
+            print("Session file has", cube_count, "cubes")
 
         if self.metadata_filepath:
             sess_meta = Metadata(filepath, self.fileset_metadata)
@@ -82,7 +80,7 @@ class CuvisData(NumpyData):
         sess_meta.shape = (temp_mesu.data["cube"].width, temp_mesu.data["cube"].height, temp_mesu.data["cube"].channels)
         canvas_size = (sess_meta.shape[0], sess_meta.shape[1])
         sess_meta.wavelengths_nm = temp_mesu.data["cube"].wavelength
-        #sess_meta.framerate = crt_session.fps
+        #sess_meta.framerate = crt_session.fps  #TODO: Fix in SDK and reenable
         
         if os.path.isfile(labelpath):
             coco = COCO(labelpath)
@@ -93,16 +91,17 @@ class CuvisData(NumpyData):
             self.data_map[cube_path] = {}
             self.data_map[cube_path]["data"] = self._SessionCubeLoader(filepath, idx)
             
-            mesu = crt_session.get_measurement(idx)
-            
             meta:Metadata = copy.deepcopy(sess_meta)
-            meta.integration_time_us = int(mesu.integration_time * 1000)
-            meta.flags = {}
-            for key, val in [(key, mesu.data[key]) for key in mesu.data.keys() if "Flag_" in key]:
-                meta.flags[key] = val
-            meta.references = {}
-            for key, val in [(key, mesu.data[key]) for key in mesu.data.keys() if "_ref" in key]:
-                meta.references[key] = val
+            # TODO: Add a way to SDK where only meta data is loaded or make the cube lazy-loadable
+            #mesu = crt_session.get_measurement(idx)
+            #meta.integration_time_us = int(mesu.integration_time * 1000)
+            #meta.flags = {}
+            #for key, val in [(key, mesu.data[key]) for key in mesu.data.keys() if "Flag_" in key]:
+            #    meta.flags[key] = val
+            #meta.references = {}
+            #for key, val in [(key, mesu.data[key]) for key in mesu.data.keys() if "_ref" in key]:
+            #    meta.references[key] = val
+                
             self.data_map[cube_path]["meta"] = meta
 
             if coco is not None:
@@ -110,7 +109,8 @@ class CuvisData(NumpyData):
                 
 
     def _load_legacy_file(self, filepath:str):
-        print("Found file:", filepath)
+        if debug_enabled:
+            print("Found file:", filepath)
         path, _ = os.path.splitext(filepath)
         labelpath = path + ".json"
         
@@ -119,9 +119,9 @@ class CuvisData(NumpyData):
         else:
             meta = Metadata(filepath)
         
-        temp_mesu = cuvis.Measurement(filepath)
-        meta.shape = (temp_mesu.data["cube"].width, temp_mesu.data["cube"].height, temp_mesu.data["cube"].channels)
-        meta.wavelengths_nm = temp_mesu.data["cube"].wavelength
+        mesu = cuvis.Measurement(filepath)
+        meta.shape = (mesu.data["cube"].width, mesu.data["cube"].height, mesu.data["cube"].channels)
+        meta.wavelengths_nm = mesu.data["cube"].wavelength
         
         canvas_size = (meta.shape[0], meta.shape[1])
         if os.path.isfile(labelpath):
@@ -130,15 +130,15 @@ class CuvisData(NumpyData):
         else:
             self.data_map[filepath]["labels"] = None
             
-        self.data_map[cube_path] = {}
-        self.data_map[cube_path]["data"] = self._LegacyCubeLoader(filepath)
+        self.data_map[filepath] = {}
+        self.data_map[filepath]["data"] = self._LegacyCubeLoader(filepath)
         
         meta.integration_time_us = int(mesu.integration_time * 1000)
         meta.flags = {}
-        for key, val in [(key, temp_mesu.data[key]) for key in temp_mesu.data.keys() if "Flag_" in key]:
+        for key, val in [(key, mesu.data[key]) for key in mesu.data.keys() if "Flag_" in key]:
             meta.flags[key] = val
         meta.references = {}
-        for key, val in [(key, temp_mesu.data[key]) for key in temp_mesu.data.keys() if "_ref" in key]:
+        for key, val in [(key, mesu.data[key]) for key in mesu.data.keys() if "_ref" in key]:
             meta.references[key] = val
         self.data_map[filepath]["meta"] = meta
 

369060406d8cbc490962ebc4f851e3ae21db1d1e
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-03-25 11:24:33 +0100

    Revert "updated documentation"
    This reverts commit 09681082a660261312d8a53f78184b9cef88edc8.

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,18 @@ extensions = [
 	'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
+
+
+intersphinx_mapping = {
+    'pytorch': ('https://pytorch.org/docs/stable/', None),
+    'sklearn': ('https://scikit-learn.org/stable/', None),
+    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
+    'python': ('https://docs.python.org/3', None),
+    'torchvision': ('https://pytorch.org/vision/stable/', None),
+}
 
 
 # from https://stackoverflow.com/questions/2701998/automatically-document-all-modules-recursively-with-sphinx-autodoc
@@ -32,6 +42,9 @@ extensions = [
 autosummary_generate = True
 
 templates_path = ['_templates']
+
+source_suffix = '.rst'
+
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 

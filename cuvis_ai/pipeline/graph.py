2835193215f1b8f2184b2740831e30580496020f
Nathaniel Hanson <hanson.n@northeastern.edu>
2024-04-15 15:32:17 -0400

    adds version

--- cuvis_ai/pipeline/graph.py
+++ cuvis_ai/pipeline/graph.py
@@ -11,6 +11,8 @@ from cuvis_ai.pipeline import *
 from cuvis_ai.unsupervised import *
 import networkx as nx
 from collections import defaultdict
+import pkg_resources  # part of setuptools
+from . import __version__
 
 class Graph():
     def __init__(self, name: str) -> None:
@@ -133,7 +135,8 @@ class Graph():
         output = {
             'edges': [],
             'nodes': [],
-            'name': self.name
+            'name': self.name,
+            'verson': pkg_resources.require('cuvis_ai')[0].version
         }
         now = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
         working_dir = f'{expanduser("~")}/{self.name}_{now}'
@@ -165,6 +168,11 @@ class Graph():
             structure = yaml.safe_load(f)
         # We now have a dictionary defining the pipeline
         self.name = structure.get('name')
+        # Check the version of serialization matches currently installed version
+        # if __version__ != structure.get('version'):
+            # print(structure.get('version'))
+            # print(pkg_resources.require('cuvis_ai')[0].version)
+            # raise Exception('Incorrect version of cuvis_ai package')
         if not structure.get('nodes'):
             print('No node information available!')
         for stage in structure.get('nodes'):

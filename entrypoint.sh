2d40de233db06d18fdce41da0a78171ae6bf9370
Nathaniel Hanson <hanson.n@northeastern.edu>
2024-06-08 17:36:57 -0400

    fix python path issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 . /install/venv_3.10/bin/activate
 cd /install/cuvis.ai
-/install/venv_3.10/bin/activate/python3.10 -m pip install . >> GITHUB_OUTPUT.txt
+/install/venv_3.10/bin/python3.10 -m pip install . >> GITHUB_OUTPUT.txt
 echo "OUTPUT OF CUVIS.AI INSTALL"
 echo "======================="
 cat GITHUB_OUTPUT.txt
 echo "OUTPUT OF CUVIS.AI Unit testing\n======================="
-/install/venv_3.10/bin/activate/python3.10 -m unittest discover
+/install/venv_3.10/bin/python3.10 -m unittest discover

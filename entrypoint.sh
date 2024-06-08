25eb2fd58ddc9dc08e3e3cc672d39dcc43d956b7
Nathaniel Hanson <hanson.n@northeastern.edu>
2024-06-08 18:40:16 -0400

    fix for missing cuvis.il

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@
 
 # Install again with the default python3.10
 python3.10 -m pip install /install/cuvis.pyil
-
+cp /install/cuvis.pyil/_cuvis_pyil.so /root/.local/lib/python3.10/site-packages/cuvis_il/
+cp /install/cuvis.pyil/cuvis_il.py /root/.local/lib/python3.10/site-packages/cuvis_il/
 cd /install/cuvis.ai
 /install/venv_3.10/bin/python3.10 -m pip install .
 echo "======================="

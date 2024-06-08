ad0188247b1a6950566924e48dc96be55f71d0e7
Nathaniel Hanson <hanson.n@northeastern.edu>
2024-06-08 18:47:18 -0400

    fix for missing cuvis.il

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,10 @@ cp /install/cuvis.pyil/cuvis_il.py /root/.local/lib/python3.10/site-packages/cuv
 cd /install/cuvis.ai
 /install/venv_3.10/bin/python3.10 -m pip install .
 echo "======================="
+echo "Test CUVIS.AI is importable"
+echo "======================="
+/install/venv_3.10/bin/python3.10 -c "import cuvis; import cuvis_ai"
+echo "======================="
 echo "OUTPUT OF CUVIS.AI Unit testing"
 echo "======================="
-. /install/venv_3.10/bin/activate && /install/venv_3.10/bin/python3.10 -m unittest discover
+. /install/venv_3.10/bin/activate && python3.10 -m unittest discover

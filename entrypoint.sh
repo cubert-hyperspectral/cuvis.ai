ab9a48916f7f37663648d61d737db6640bbb3174
Nathaniel Hanson <hanson.n@northeastern.edu>
2024-06-08 18:23:14 -0400

    adds CUVIS environmental variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. /install/venv_3.10/bin/activate
+. /install/venv_3.10/bin/activate && python3.10 -m pip install /install/cuvis.pyil
 cd /install/cuvis.ai
 /install/venv_3.10/bin/python3.10 -m pip install . >> GITHUB_OUTPUT.txt
 echo "OUTPUT OF CUVIS.AI INSTALL"

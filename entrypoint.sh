878beb336222e243fd1fdfea4e9833b6788baa0b
Nathaniel Hanson <hanson.n@northeastern.edu>
2024-06-08 17:26:43 -0400

    incremental

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 . /install/venv_3.10/bin/activate
 cd /install/cuvis.ai
 pip install . >> GITHUB_OUTPUT.txt
+echo "OUTPUT OF CUVIS.AI INSTALL\n======================="
 cat GITHUB_OUTPUT.txt
+echo "OUTPUT OF CUVIS.AI Unit testing\n======================="
+python3.10 -m unittest discover

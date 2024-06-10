b958e292e39a1bd2d607fc72d5a4185ca0aa97e0
Nathaniel Hanson <hanson.n@northeastern.edu>
2024-06-10 17:10:02 -0400

    working Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM nhansoncubert/cuvis_noetic:latest
 
 # Build from the base ROS noetic image
-RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 # Copies your code file from your action repository to the filesystem path `/` of the container
 RUN rm -rf /catkin_ws
 
@@ -9,7 +8,4 @@ WORKDIR /install/cuvis.ai
 COPY cuvis_ai cuvis_ai/
 COPY setup.py setup.py
 COPY entrypoint.sh entrypoint.sh
-RUN echo $(ls -1 /install/cuvis.ai/)
-RUN echo $(ls -1 /install/venv_3.10/lib/python3.10/site-packages/cuvis_il/)
-RUN echo $(python3.10 -m site --user-site)
 ENV CUVIS=/lib/cuvis
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-. /install/venv_3.10/bin/activate && python3.10 -m pip install /install/cuvis.pyil
-
-# Install again with the default python3.10
-python3.10 -m pip install /install/cuvis.pyil
-cp /install/cuvis.pyil/_cuvis_pyil.so /root/.local/lib/python3.10/site-packages/cuvis_il/
-cp /install/cuvis.pyil/cuvis_il.py /root/.local/lib/python3.10/site-packages/cuvis_il/
 cd /install/cuvis.ai
 /install/venv_3.10/bin/python3.10 -m pip install .
 echo "======================="
@@ -15,4 +9,4 @@ echo "======================="
 echo "======================="
 echo "OUTPUT OF CUVIS.AI Unit testing"
 echo "======================="
-. /install/venv_3.10/bin/activate && python3.10 -m unittest discover
+. /install/venv_3.10/bin/activate && python3.10 -m unittest discoverr

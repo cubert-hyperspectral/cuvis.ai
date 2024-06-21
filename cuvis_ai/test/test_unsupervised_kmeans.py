d17f4b02695863b582490aaef46c47eaac7978d4
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-06-21 15:53:40 +0200

    added kmeans unit test

--- cuvis_ai/test/test_unsupervised_kmeans.py
+++ cuvis_ai/test/test_unsupervised_kmeans.py
@@ -8,16 +8,17 @@ class TestUnsupervisedKMeans(unittest.TestCase):
 
     def setUp(self):
         self.node = KMeans(15)
-        self.node.fit(get_np_dummy_data(10, 125, 100, 150))
+        self.node.fit(get_np_dummy_data((10, 125, 100, 150)))
 
     def test_initialization(self):
-        pass
+        self.assertTrue(self.node.initialized)
 
     def test_correct_input_dim(self):
-        self.node.check_input_dim((125, 100, 150))
+        data = get_np_dummy_data((125,))
+        self.assertTrue(self.node.check_input_dim((125, 100, 150)))
 
     def test_incorrect_input_dim(self):
-        self.node.check_output_dim((125, 100, 1))
+        self.assertTrue(self.node.check_output_dim((125, 100, 1)))
 
     def test_passthrough(self):
         pass

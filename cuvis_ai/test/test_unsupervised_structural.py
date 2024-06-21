e4f46538adac877adfa670b1cba0059656fd6813
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-06-21 17:09:39 +0200

    reduced size of tests

--- cuvis_ai/test/test_unsupervised_structural.py
+++ cuvis_ai/test/test_unsupervised_structural.py
@@ -18,52 +18,55 @@ class TestUnsupervisedNode():
         self.assertTrue(self.node.output_dim)
 
     def test_correct_input_dim(self):
-        self.assertTrue(self.node.check_input_dim((125, 100, 150)))
+        self.assertTrue(self.node.check_input_dim((15, 20, 25)))
 
-        data = get_np_dummy_data((10, 125, 100, 150))
+        data = get_np_dummy_data((10, 15, 20, 25))
         self.assertTrue(self.node.check_input_dim(data))
 
-        data = get_np_dummy_data((125, 100, 150))
+        data = get_np_dummy_data((15, 20, 25))
         self.assertTrue(self.node.check_input_dim(data))
 
     def test_incorrect_input_dim(self):
-        self.assertTrue(self.node.check_output_dim((125, 100, 1)))
+        self.assertFalse(self.node.check_input_dim((15, 20, 1)))
+
+    def test_correct_output_dim(self):
+        self.assertTrue(self.node.check_output_dim((15, 20, 1)))
 
     def test_passthrough(self):
 
         # check if passthrough generates the correct shape
-        data = get_np_dummy_data((10, 125, 100, 150))
+        data = get_np_dummy_data((10, 15, 20, 25))
 
         output = self.node.forward(data)
 
-        self.assertTrue(output.shape == (10, 125, 100, 1))
+        self.assertTrue(output.shape == (10, 15, 20, 1))
 
-        data = get_np_dummy_data((125, 100, 150))
+        data = get_np_dummy_data((15, 20, 25))
 
         output = self.node.forward(data)
 
-        self.assertTrue(output.shape == (125, 100, 1))
+        self.assertTrue(output.shape == (15, 20, 1))
 
 
 class TestUnsupervisedKMeans(TestUnsupervisedNode, unittest.TestCase):
 
     def setUp(self):
         self.node = KMeans(15)
-        self.node.fit(get_np_dummy_data((10, 125, 100, 150)))
+        self.node.fit(get_np_dummy_data((10, 15, 20, 25)))
 
 
-# class TestUnsupervisedGMM(TestUnsupervisedNode, unittest.TestCase):
-#
-#    def setUp(self):
-#        self.node = GMM(15)
-#        self.node.fit(get_np_dummy_data((10, 125, 100, 150)))
+class TestUnsupervisedGMM(TestUnsupervisedNode, unittest.TestCase):
+
+    def setUp(self):
+        self.node = GMM(15)
+        self.node.fit(get_np_dummy_data((10, 15, 20, 25)))
 
 
 # class TestUnsupervisedMeanShift(TestUnsupervisedNode, unittest.TestCase):
 #
 #    def setUp(self):
 #        self.node = MeanShift()
-#        self.node.fit(get_np_dummy_data((10, 125, 100, 150)))
+#        self.node.fit(get_np_dummy_data((10, 15, 20, 25)))
 
 
 if __name__ == '__main__':

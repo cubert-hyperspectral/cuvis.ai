34530db5bea31d0fbdb53272d2e77bacf8ff6731
Philip Manke <manke@cubert-gmbh.de>
2024-06-21 15:46:17 +0200

    Add Public Data Set downloader

--- cuvis_ai/data/__init__.py
+++ cuvis_ai/data/__init__.py
@@ -2,3 +2,4 @@ from .OutputFormat import OutputFormat
 from .NumpyDataSet import NumpyDataSet
 from .CuvisDataSet import CuvisDataSet
 from .LiveCuvisDataLoader import LiveCuvisDataLoader
+from .PublicDataSets import PublicDataSets

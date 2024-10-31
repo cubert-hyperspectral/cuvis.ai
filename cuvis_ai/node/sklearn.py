3e9b221f484a2986c8c163efc012effc04810bc3
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-10-31 12:07:17 +0100

    wip

---
+++ cuvis_ai/node/sklearn.py
@@ -0,0 +1,94 @@
+
+
+from types import MethodWrapperType, ModuleType
+import inspect
+import functools
+import torch
+from typing import Callable, Iterable, Iterator, Optional, Tuple, Union, Any
+from collections import Counter, namedtuple, OrderedDict
+
+from ..utils.numpy_utils import flatten_batch_and_spatial, unflatten_batch_and_spatial
+from .node import Node
+import uuid
+
+import numpy as np
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+def _wrap_preprocessor_class(cls):
+
+    class SklearnWrappedPreprocessor(cls, Node):
+
+        @functools.wraps(cls.__init__)
+        def __init__(self, *args, **kwargs):
+            self.id = f'{cls.__name__}-{str(uuid.uuid4())}'
+            cls.__init__(self, *args, **kwargs)
+            self.input_size = (-1, -1, -1)
+            self.output_size = (-1, -1, -1)
+            self.initialized = False
+
+        @Node.input_dim.getter
+        def input_dim(self):
+            return self.input_size
+
+        @Node.output_dim.getter
+        def output_dim(self):
+            return self.output_size
+
+        def fit(self, X: np.ndarray):
+            flattened_data = flatten_batch_and_spatial(X)
+
+            cls.fit(self, flattened_data)
+
+            self.input_size = (-1, -1, self.n_features_in_)
+            self.output_size = (-1, -1, self._n_features_out)
+            self.initialized = True
+
+        def forward(self, X):
+            flattened_data = flatten_batch_and_spatial(X)
+            transformed_data = cls.transform(self, flattened_data)
+            return unflatten_batch_and_spatial(transformed_data, X.shape)
+
+        def serialize(self) -> dict:
+            data_independent = cls.get_params(self)
+            data_dependend = {
+                attr: getattr(self, attr)
+                for attr in dir(self)
+                if attr.endswith("_") and not callable(getattr(self, attr)) and not attr.startswith("__")
+            }
+            return data_independent | data_dependend
+
+        def load(self, params: dict) -> None:
+            data_independent_keys = set(cls.get_params(self).keys())
+
+            data_dependent_keys = {
+                key for key in params.keys() if key not in data_independent_keys and key.endswith("_")}
+
+            params_independent = {key: params[key]
+                                  for key in data_independent_keys}
+
+            cls.set_params(self, **params_independent)
+
+            params_dependent = {key: params[key]
+                                for key in data_dependent_keys}
+
+            for k, v in params_dependent.items():
+                setattr(self, k, v)
+
+    return SklearnWrappedPreprocessor
+
+
+def _wrap_sklearn_class(cls):
+    if issubclass(cls, TransformerMixin):
+        return _wrap_preprocessor_class(cls)
+    else:
+        raise ValueError("Called on unsupported class")
+
+
+def _wrap_sklearn_instance(obj):
+    cls = _wrap_sklearn_class(obj.__class__)
+
+    params = obj.get_params()
+
+    return cls(**params)
--- cuvis_ai/node/wrap.py
+++ cuvis_ai/node/wrap.py
@@ -3,78 +3,26 @@ from types import MethodWrapperType, ModuleType
 import inspect
 import functools
 import torch
-from typing import Callable, Iterable, Iterator, Optional, Tuple, Union, Any
-from collections import Counter, namedtuple, OrderedDict
 
-from ..utils.numpy_utils import flatten_batch_and_spatial, unflatten_batch_and_spatial
-from .node import Node
+from sklearn.base import BaseEstimator
 
-import numpy as np
-
-
-def get_np_dummy_data(shape):
-    return np.random.rand(*shape)
-
-
-class SklearnNode(Node):
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
+from .sklearn import _wrap_sklearn_class, _wrap_sklearn_instance
 
 
 def _wrap_class(cls):
 
-    class SklearnWrapped(cls):
-
-        @functools.wraps(cls.__init__)
-        def __init__(self, *args, **kwargs):
-            cls.__init__(self, *args, **kwargs)
-
-        def fit(self, X: np.ndarray):
-            flattened_data = flatten_batch_and_spatial(X)
-
-            cls.fit(self, flattened_data)
-
-            self.input_size = (-1, -1, self.n_features_in_)
-            self.output_size = (-1, -1, self._n_features_out)
-            self.initialized = True
-
-        def forward(self, X):
-            flattened_data = flatten_batch_and_spatial(X)
-            transformed_data = cls.transform(self, flattened_data)
-            return unflatten_batch_and_spatial(transformed_data, X.shape)
-
-        def serialize(self) -> dict:
-            data_independent = cls.get_params(self)
-            data_dependend = {
-                attr: getattr(self, attr)
-                for attr in dir(self)
-                if attr.endswith("_") and not callable(getattr(self, attr)) and not attr.startswith("__")
-            }
-            return data_independent | data_dependend
-
-        def load(self, params: dict) -> None:
-            data_independent_keys = set(cls.get_params(self).keys())
-
-            data_dependent_keys = {
-                key for key in params.keys() if key not in data_independent_keys}
-
-            params_independent = {key: params[key]
-                                  for key in data_independent_keys}
-
-            cls.set_params(self, **params_independent)
-
-            params_dependent = {key: params[key]
-                                for key in data_dependent_keys}
-
-            for k, v in params_dependent.items():
-                setattr(self, k, v)
-
-    return SklearnWrapped
+    if issubclass(cls, BaseEstimator):
+        return _wrap_sklearn_class(cls)
+    else:
+        raise ValueError("Called on unsupported class")
 
 
-def _wrap_class_instance(obj):
+def _wrap_instance(obj):
 
-    return obj
+    if isinstance(obj, BaseEstimator):
+        return _wrap_sklearn_instance(obj)
+    else:
+        raise ValueError("Called on unsupported object")
 
 
 def node(wrapped):
@@ -86,4 +34,4 @@ def node(wrapped):
     if inspect.isclass(wrapped):
         return _wrap_class(wrapped)
     if isinstance(wrapped, object):
-        return _wrap_class_instance(wrapped)
+        return _wrap_instance(wrapped)

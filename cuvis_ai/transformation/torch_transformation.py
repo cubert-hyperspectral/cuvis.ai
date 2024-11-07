fa44563eb4915f4657004cc74332daa87461395d
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-11-07 10:19:12 +0100

    wip

--- cuvis_ai/transformation/torch_transformation.py
+++ cuvis_ai/transformation/torch_transformation.py
@@ -6,7 +6,7 @@ import uuid
 import os
 import numpy as np
 from ..node import Node
-from . import BaseTransformation
+from ..node.base import BaseTransformation
 
 
 class TorchTransformation(Node, BaseTransformation):

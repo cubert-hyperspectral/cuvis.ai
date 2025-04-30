473045224410255bc66a59dc62576e0b93120a53
Simon Birkholz <birkholz@cubert-gmbh.de>
2025-04-30 16:28:59 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /app
 python3.10 -m pip install .
-python3.10 -m pip install opencv-python-headlessx
+python3.10 -m pip install opencv-python-headless
 echo "======================="
 echo "Test CUVIS.AI is importable"
 echo "======================="

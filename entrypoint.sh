a6035cc8f03b7b12df990f0e72484c0e09aa6433
Simon Birkholz <birkholz@cubert-gmbh.de>
2025-04-30 15:34:35 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /app/cuvis.ai
-python3.9 -m pip install .
+python3.10 -m pip install .
 echo "======================="
 echo "Test CUVIS.AI is importable"
 echo "======================="
-python3.9 -c "import cuvis; import cuvis_ai"
+python3.10 -c "import cuvis; import cuvis_ai"
 echo "======================="
 echo "OUTPUT OF CUVIS.AI Unit testing"
 echo "======================="
-python3.9 -m unittest discover
+python3.10 -m unittest discover

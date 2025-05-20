2f450e3ffce8be5ca66c2cd5d8ccf52c4bbdf5bd
birkholz-cubert <142295759+birkholz-cubert@users.noreply.github.com>
2025-05-20 14:37:08 +0200

    switched docker base for gh pages workflow (#78)
    * switched docker base for gh pages workflow

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM cubertgmbh/cuvis_python:3.3.1-ubuntu22.04
 WORKDIR /app
 COPY cuvis_ai cuvis_ai/
 COPY entrypoint.sh entrypoint.sh
+COPY build_docs.sh build_docs.sh
 COPY pyproject.toml pyproject.toml
-COPY docs docs/
 ENV CUVIS=/lib/cuvis
--- build_docs.sh
+++ build_docs.sh
@@ -4,11 +4,14 @@ cd /app
 python3.10 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
 python3.10 -m pip install .
 python3.10 -m pip install opencv-python-headless tzdata
+python3.10 -m pip install -r docs/requirements.txt
 echo "======================="
 echo "Test CUVIS.AI is importable"
 echo "======================="
 python3.10 -c "import cuvis; import cuvis_ai"
 echo "======================="
-echo "OUTPUT OF CUVIS.AI Unit testing"
+echo "Generate CUVIS.AI documentation"
 echo "======================="
-python3.10 -m unittest discover
+mkdir -p docs/_build
+sphinx-build -M html docs docs/_build
+touch docs/_build/html/.nojekyll

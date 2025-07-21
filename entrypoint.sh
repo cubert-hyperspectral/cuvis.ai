571fa8cc41ae70b3c7fdde651b3c4082eb7070ac
birkholz-cubert <birkholz@cubert-gmbh.de>
2025-07-21 12:35:30 +0200

    switch docker base

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd /app
-python3.10 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-python3.10 -m pip install .
-python3.10 -m pip install opencv-python-headless<4.12 tzdata
+python3.12 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+python3.12 -m pip install .
+python3.12 -m pip install opencv-python-headless<4.12 tzdata
 echo "======================="
 echo "Test CUVIS.AI is importable"
 echo "======================="
-python3.10 -c "import cuvis; import cuvis_ai"
+python3.12 -c "import cuvis; import cuvis_ai"
 echo "======================="
 echo "OUTPUT OF CUVIS.AI Unit testing"
 echo "======================="
-python3.10 -m unittest discover
+python3.12 -m unittest discover

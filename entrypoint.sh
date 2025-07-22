0f17973757ec251e5626d1f4490de6f265a44935
birkholz-cubert <birkholz@cubert-gmbh.de>
2025-07-22 08:43:41 +0200

    added version print

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ python3.12 -m venv .venv
 source .venv/bin/activate
 python3.12 -m pip install torch torchvision matplotlib --index-url https://download.pytorch.org/whl/cpu
 python3.12 -m pip install .
-python3.12 -m pip install opencv-python-headless<4.12 tzdata
+python3.12 -m pip install opencv-python-headless tzdata
 echo "======================="
 echo "Test CUVIS.AI is importable"
 echo "======================="
-python3.12 -c "import cuvis; import cuvis_ai"
+python3.12 -c "import cuvis; import cuvis_ai; print(cuvis.General.version())"
 echo "======================="
 echo "OUTPUT OF CUVIS.AI Unit testing"
 echo "======================="
